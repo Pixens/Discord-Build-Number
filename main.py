dda392b3f175bd66b4b75b738294653818ed5242
Pixens <107793019+Pixens@users.noreply.github.com>
2023-10-27 18:15:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ def get_build_number():
     build_number = 240884
     try:
         login_response = requests.get('https://discord.com/login')
-        file = f'https://discord.com/assets/{re.compile(r"assets/+([a-z0-9]+)\.js").findall(login_response.text)[-2]}.js'
+        file = 'https://discord.com/assets/' + re.compile(r"assets/+([a-z0-9]+)\.js").findall(login_response.text)[-2] + '.js'
         file_response = requests.get(file)
         build_number = file_response.text.split('Build Number: ").concat("')[1].split('",')[0]
     except Exception:
         pass
-      
     return build_number
