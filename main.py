0c5eeebed4cb8325a7f4d9cbdedc02b619cfb2fd
Pixens <vihaang2801@gmail.com>
2024-05-08 20:33:04 +0530

    Updated build number fetcher

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tls_client
+import re
 
 
 headers = {
@@ -16,17 +17,29 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 }
 
+session = tls_client.Session(client_identifier="chrome_124")
+def extract_asset_files():
+    request = session.get("https://discord.com/login", headers=headers)
+    pattern = r'<script\s+src="([^"]+\.js)"\s+defer>\s*</script>'
+    matches = re.findall(pattern, request.text)
+    return matches
 
 def get_build_number():
     try:
-        session = tls_client.Session(client_identifier="chrome_124")
-        build_url = "https://discord.com/assets/84471.3244a3093dadec43fc23.js"
-        response = session.get(build_url, headers=headers)
-        build_number = response.text.split('"buildNumber",(_="')[1].split('"')[0]
+        files = extract_asset_files()
+        for file in files:
+            build_url = f"https://discord.com/{file}"
+            response = session.get(build_url, headers=headers)
+            if "buildNumber" not in response.text:
+                continue
+            else:
+                build_number = response.text.split('"buildNumber",(_="')[1].split('"')[0]
+                break
+
         session.close()
         return build_number
-    except Exception:
-        return 289814
+    except Exception as e:
+        return 290453
 
 
 discord_build_number = get_build_number()
